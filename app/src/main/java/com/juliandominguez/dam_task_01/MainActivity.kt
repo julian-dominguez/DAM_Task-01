369cdebba97aaf779518b5ebb1a5f5e371e083d3
Julian D_D <julian.dominguez@correounivalle.edu.co>
2021-03-03 15:38:34 -0500

    Se elimina contenido para hacer uso del constrain layout

--- app/src/main/java/com/juliandominguez/dam_task_01/MainActivity.kt
+++ app/src/main/java/com/juliandominguez/dam_task_01/MainActivity.kt
@@ -6,7 +6,6 @@ import com.google.android.material.snackbar.Snackbar
 import com.juliandominguez.dam_task_01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     /**
      * Mejora del manejo de findViewById
      * viewBinding permite solo acceder a los elementos de la vista asociada a la
@@ -18,32 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-
-        /**
-         * Se activa ek evento de clic en en btn send
-         */
-        this.viewBinding.btnSend.setOnClickListener {
-
-            /**
-             * Se captura el texto que fue digitado en edHeroe y se almacena en una
-             * variable al hacer clic en el btn
-             */
-            var strHeroe: String = viewBinding.etNameHeroe.text.toString()
-
-            /**
-             * Se crea la notificación de forma antigua con Toast
-             */
-            // Toast.makeText(this,"Has enviado el valor: $strHeroe de forma correcta", Toast.LENGTH_LONG).show()
-
-            /**
-             * Se crea notificación de forma actual Snackbar
-             */
-            Snackbar.make(
-                it,
-                getString(R.string.notification, strHeroe),
-                Snackbar.LENGTH_LONG
-            ).setAnchorView(R.id.btn_send).show()
-        }
 
     }
 }
