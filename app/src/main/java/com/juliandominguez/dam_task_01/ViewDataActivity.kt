561f77a741c5b201b25f0b2699d218e04a9f5c2f
Julian D_D <julian.dominguez@correounivalle.edu.co>
2021-03-03 18:20:39 -0500

    Se crea nuevo activity < activity_view_data > y se declara en el manifest

---
+++ app/src/main/java/com/juliandominguez/dam_task_01/ViewDataActivity.kt
@@ -0,0 +1,11 @@
+package com.juliandominguez.dam_task_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_data)
+    }
+}
