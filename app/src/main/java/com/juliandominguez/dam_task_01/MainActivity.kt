7f9a0f143cd114cbe49e9b187c0dbf71ee31fd06
Julian D_D <julian.dominguez@correounivalle.edu.co>
2021-03-04 19:17:56 -0500

    Se termina implementación de intent explícito

--- app/src/main/java/com/juliandominguez/dam_task_01/MainActivity.kt
+++ app/src/main/java/com/juliandominguez/dam_task_01/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.juliandominguez.dam_task_01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import com.juliandominguez.dam_task_01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    /**
-     * Mejora del manejo de findViewById
-     * viewBinding permite solo acceder a los elementos de la vista asociada a la
-     * actividad, evitando así sobre cargas o acceso indeseado a elementos de otras actividades
-     */
+
     private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        viewBinding.btnSend.setOnClickListener {
+
+
+            val intent = Intent(this, ViewDataActivity::class.java)
+
+            /**
+             * Se capturan los datos de la vista principal
+             */
+            val nameHero: String = viewBinding.etNameHeroe.text.toString()
+            val powerHero: Float = viewBinding.rbRatingPower.rating
+
+            /**
+             * Se almacenan los valores para pasarlos de la activity principal a la secundaria.
+             */
+            intent.putExtra(ViewDataActivity.HERO_HEY, nameHero)
+            intent.putExtra(ViewDataActivity.POWER_HERO_KEY, powerHero)
+            /**
+             * Se lanza la segunda vista
+             */
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/juliandominguez/dam_task_01/ViewDataActivity.kt
+++ app/src/main/java/com/juliandominguez/dam_task_01/ViewDataActivity.kt
@@ -2,10 +2,35 @@ package com.juliandominguez.dam_task_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.juliandominguez.dam_task_01.databinding.ActivityViewDataBinding
 
 class ViewDataActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityViewDataBinding
+
+    /**
+     * Se crean constantes con las llaves de los datos del intent
+     */
+
+    companion object{
+        const val HERO_HEY = "hero_key"
+        const val POWER_HERO_KEY = "power_hero_key"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_data)
+        viewBinding = ActivityViewDataBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        /**
+         * Tomo los valores que vienen de la activity principal
+         */
+        val intent:Bundle = intent.extras!!
+
+        /**
+         * Se setean los valores en la vista
+         */
+        viewBinding.tvNameHeroA2.text = intent.getString(HERO_HEY)
+        viewBinding.rbPowerRatingA2.rating = intent.getFloat(POWER_HERO_KEY)
     }
 }
