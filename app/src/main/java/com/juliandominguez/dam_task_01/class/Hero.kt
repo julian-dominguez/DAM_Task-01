ba2a575adf80de0e79861849b1284a64ccab4ab6
Julian D_D <julian.dominguez@correounivalle.edu.co>
2021-03-06 16:03:39 -0500

    Se eliminan lineas de código no utilizadas

--- app/src/main/java/com/juliandominguez/dam_task_01/class/Hero.kt
+++ app/src/main/java/com/juliandominguez/dam_task_01/class/Hero.kt
@@ -4,13 +4,12 @@ import android.graphics.Bitmap
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Hero(val nameHero: String, val ratingPower: Float, val photo: Bitmap) : Parcelable {
+data class Hero (val nameHero: String, val ratingPower: Float, val photo: Bitmap) : Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readString().toString(),
         parcel.readFloat(),
         parcel.readParcelable(Bitmap::class.java.classLoader)!!
-    ) {
-    }
+    )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(nameHero)
